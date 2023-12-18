001f86cc5ac641792a3df13075f0ee96e5158e86
kellyytruong <143736559+kellyytruong@users.noreply.github.com>
2023-12-17 23:27:37 -0700

    js-final

--- js-project/js.js
+++ js-project/js.js
@@ -1,24 +1,74 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const ballContainer = document.getElementById('ballContainer');
-    const phoneNumberInput = document.getElementById('phoneNumber');
+function $(element) {
+    if(document.querySelectorAll(element).length !== 1) {
+        return document.querySelectorAll(element);
+    } else {
+        return document.querySelector(element)
+    }
+}
+const numFormat = "(===) === - ===="
+const numLength = 10;
+let points = 0;
 
-    function createBall(number) {
-        let ball = document.createElement('div');
-        ball.className = 'ball';
-        ball.textContent = number;
-        ball.style.left = `${10 + number * 8}%`; // Spread out the balls
-        ball.style.animationDuration = `1s`; // Consistent faster speed for all balls
+
+/* Format the Numbers */
+function formatNumber() {
+    let pts = points.toString()
+    let number = numFormat
     
-        ball.addEventListener('click', () => {
-            phoneNumberInput.value += number;
-        });
-    
-        ballContainer.appendChild(ball);
+    // add leading zeros
+    while(pts.length < numLength) {
+        pts = "0" + pts
     }
 
-    for (let i = 0; i < 10; i++) {
-        createBall(i);
+    // apply to numFormat
+    pts.split("").forEach(num => {
+        number = number.replace("=", num)
+    })
+
+    return number;
+}
+
+/* Holes */
+let x = setInterval(function() {
+    let point = Math.floor(Math.random() * 8);
+    $(".hole")[point].style.background = "#ffa100";
+    setTimeout(function() {
+        $(".hole")[point].style.background = "";
+    }, 725)
+}, 925)
+
+
+
+/* Add function when clicked, adds a 'point' */
+$(".hole").forEach(hole => {
+    hole.addEventListener("click", () => {
+        if(hole.style.background == "#ffa100" || hole.style.background == "rgb(255, 161, 0)") {
+            points++;
+            $(".number").innerHTML = formatNumber();
+            hole.style.background = ""
+        }
+    })
+})
+
+
+
+/* Mark as done */
+document.addEventListener("keydown", (e) => {
+    if(e.key.toLowerCase() == "enter") {
+        $("h2").innerHTML = "Submitting your number as " + $(".number").innerHTML + ". Thank you!"
+        clearInterval(x);
+        $(".game").style.display = "none"
     }
-});
+})
+
+document.addEventListener("touchstart", (e) => {
+    if(!e.path.includes($(".activity"))) {
+        document.dispatchEvent(new KeyboardEvent("keydown", {key: "Enter"}))
+    }
+})
+
+
+
+
 
 
